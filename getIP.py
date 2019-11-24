0d1d60fe240d0b3c5cc32c613151d50541b8f226
qdgtruie <y.saule@gmail.com>
2019-11-24 20:47:16 +0100

    initial import from legacy repo

---
+++ getIP.py
@@ -0,0 +1,18 @@
+import socket
+
+class GetIP:
+    def get_ip(self):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            IP = s.getsockname()[0]
+        except:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+        return IP
+
+
+print("Program starting")
+print(GetIP().get_ip())
